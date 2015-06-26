4654be33a09a0fcf46f90a9e360a160e3ebd18fd
unknown <523138@NRM-U523138L002.ad.seagate.com>
2015-06-26 13:42:06 -0500

    Restart replacing GLUT with SFML

---
+++ build/CMakeFiles/Viewer3d.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Viewer3d.dir/src/main.cpp.obj"
+  "../bin/Viewer3d.pdb"
+  "../bin/Viewer3d.exe"
+  "../bin/Viewer3d.exe.manifest"
+  "../bin/libViewer3d.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Viewer3d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
